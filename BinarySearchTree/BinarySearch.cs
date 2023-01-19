8a4912cc4c7351ca255366a1048108448f0a096d
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-19 23:31:26 +0530

    Added Creating Binary Search Tree Program

---
+++ BinarySearchTree/BinarySearch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinarySearch<T> where T : IComparable<T>
+    {
+
+        public T Data;
+        public BinarySearch<T> left;
+        public BinarySearch<T> right;
+        public BinarySearch(T data)
+        {
+            this.Data = data;
+            this.left = null;
+            this.right = null;
+        }
+        int leftIndex = 0, rightIndex = 0;
+        public void AddNode(T value)
+        {
+            T Nodevalue = this.Data;
+            if (Nodevalue.CompareTo(value) > 0)
+            {
+                if (this.left == null) this.left = new BinarySearch<T>(value);
+                else
+                    this.left.AddNode(value);
+
+            }
+            else
+            {
+                if (this.right == null) this.right = new BinarySearch<T>(value);
+                else
+                    this.right.AddNode(value);
+            }
+
+        }
+        public void Display()
+        {
+            if (this.left != null)
+            {
+                this.leftIndex++;
+                this.left.Display();
+            }
+            Console.WriteLine(this.Data);
+            if (this.right != null)
+            {
+                this.rightIndex++;
+                this.right.Display();
+
+            }
+
+        }
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -5,6 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Displaying Binary Search Tree Program");
+            BinarySearch<int> binarySearch = new BinarySearch<int>(56);
+            binarySearch.AddNode(30);
+            binarySearch.AddNode(70);
+            binarySearch.Display();
         }
     }
 }
